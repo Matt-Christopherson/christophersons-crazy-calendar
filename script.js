ae6a1121a57d3cee84073d739fb5950e7f329f0f
Matt Christopherson <matthew.christopherson27@gmail.com>
2024-01-29 13:24:47 -0600

    Used dayjs to display the current day to the user

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// 2010-12-31
+
 $(function () {
+// Displays the current day using dayjs.  
+let currentDay = document.getElementById("currentDay");
+currentDay.innerText = dayjs().format("dddd, MMMM DD");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
